e6efee36f1e89e5a706cc71dbb3c0f9c2c905d62
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-01-22 02:26:55 +0530

    changes

--- src/app/(protected-routes)/stock/page.tsx
+++ src/app/(protected-routes)/stock/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return <div className="h-screen"></div>;
+    return (
+        <div className="h-screen">
+            <h1 className="text-2xl font-bold">Stock</h1>
+        </div>
+    );
 }
