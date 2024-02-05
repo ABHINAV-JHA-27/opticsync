8192ab9a70b581d45d43e88e15aaa470049bab77
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-02-05 12:45:04 +0530

    changes

--- src/app/(protected-routes)/layout.tsx
+++ src/app/(protected-routes)/layout.tsx
@@ -1,11 +1,19 @@
 import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const { isAuthenticated } = getKindeServerSession();
+
+    if (!(await isAuthenticated())) {
+        redirect("/api/auth/login?post_login_redirect_url=/dashboard");
+    }
+
     return (
         <html lang="en">
             <body>
--- src/app/api/(protected-routes)/products/route.ts
+++ src/app/api/(protected-routes)/products/route.ts
@@ -1,9 +1,18 @@
 import dbConnection from "@/lib/dbConnect";
 import Product from "@/models/product";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     await dbConnection();
+    const { getUser, isAuthenticated } = getKindeServerSession();
+
+    if (!(await isAuthenticated())) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const user = await getUser();
+    console.log(user);
 
     const products = await Product.find({});
 
