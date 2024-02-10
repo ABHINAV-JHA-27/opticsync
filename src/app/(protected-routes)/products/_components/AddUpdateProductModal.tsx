616d346d4e89051d43b9d19142b83ec8d831a92e
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-02-10 15:24:38 +0530

    Added add update product modal

--- src/app/(protected-routes)/products/_components/AddUpdateProductModal.tsx
+++ src/app/(protected-routes)/products/_components/AddUpdateProductModal.tsx
@@ -5,13 +5,43 @@ import {
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 type AddProductModalProps = {
     isOpen: boolean;
     onClose: () => void;
+    data?: any;
 };
 
 const AddUpdateProductModal = (props: AddProductModalProps) => {
+    const [productName, setProductName] = useState(
+        props.data ? props.data.name : ""
+    );
+    const [productCompany, setProductCompany] = useState(
+        props.data ? props.data.company : ""
+    );
+    const [productSrp, setProductSrp] = useState(
+        props.data ? props.data.srp : ""
+    );
+    const [productWlp, setProductWlp] = useState(
+        props.data ? props.data.wlp : ""
+    );
+
+    const handleProductSave = () => {
+        if (props.data) {
+            // Update Product
+        } else {
+            // Add Product
+        }
+        console.log({
+            productName,
+            productCompany,
+            productSrp,
+            productWlp,
+        });
+    };
+
     return (
         <Dialog
             open={props.isOpen}
@@ -21,11 +51,68 @@ const AddUpdateProductModal = (props: AddProductModalProps) => {
         >
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogTitle>
+                        {props.data ? "Edit Product" : "Add Product"}
+                    </DialogTitle>
                     <DialogDescription>
-                        This action cannot be undone. This will permanently
-                        delete your account and remove your data from our
-                        servers.
+                        <div className="w-full mt-2">
+                            <div className="w-full">
+                                <span className="text-xs font-semibold">
+                                    Product Name
+                                </span>
+                                <Input
+                                    value={productName}
+                                    onChange={(e) => {
+                                        setProductName(e.target.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="w-full mt-2">
+                                <span className="text-xs font-semibold">
+                                    Product Company
+                                </span>
+                                <Input
+                                    value={productCompany}
+                                    onChange={(e) => {
+                                        setProductCompany(e.target.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="w-full flex flex-row items-center mt-2 gap-x-2">
+                                <div className="w-1/2">
+                                    <span className="text-xs font-semibold">
+                                        SRP
+                                    </span>
+                                    <Input
+                                        value={productSrp}
+                                        onChange={(e) => {
+                                            setProductSrp(e.target.value);
+                                        }}
+                                    />
+                                </div>
+                                <div className="w-1/2">
+                                    <span className="text-xs font-semibold">
+                                        WLP
+                                    </span>
+                                    <Input
+                                        value={productWlp}
+                                        onChange={(e) => {
+                                            setProductWlp(e.target.value);
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-full mt-4 flex justify-end">
+                                <button
+                                    className="bg-primary text-white px-4 py-2 rounded-md"
+                                    onClick={handleProductSave}
+                                >
+                                    {props.data
+                                        ? "Update Product"
+                                        : "Add Product"}
+                                </button>
+                            </div>
+                        </div>
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
