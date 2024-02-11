0dc6b855ac835c5304ad8872f05530c2afe0ba56
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-02-11 11:26:42 +0530

    Added auth again

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 import { NextRequest } from "next/server";
 export default function middleware(req: NextRequest) {
-    // return withAuth(req);
-    // return req;
+    return withAuth(req);
 }
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
