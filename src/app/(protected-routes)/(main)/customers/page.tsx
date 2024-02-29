4c038b32cb9f27c75e0b88529afaeffdf76dd789
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-03-01 04:47:43 +0530

    removed unnecessary components

--- src/app/(protected-routes)/(main)/customers/page.tsx
+++ src/app/(protected-routes)/(main)/customers/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
     return (
         <div className="h-[100%] p-4">
             <div className="flex items-center justify-between mt-4">
-                <Card>top Product</Card>
-                <Card>top Product</Card>
+                {/* <Card>top Product</Card>
+                <Card>top Product</Card> */}
             </div>
             <CustomerTable />
         </div>
