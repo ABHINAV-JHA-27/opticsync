b2c34cb27d9ebc8f8c69c78d8a1b355af9e4b7c6
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-03-04 02:44:06 +0530

    Fixed navbar components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,29 +5,29 @@ const NavBar = () => {
     const pathname = usePathname();
 
     return (
-        <div className="flex flex-row items-center justify-between p-4 bg-[#ffffff]">
+        <div className="flex flex-row items-center justify-between p-4 mt-3">
             {pathname === "/dashboard" && (
-                <span className="text-[24px] font-[600] leading-none">
+                <span className="text-[36px] font-[600] leading-none">
                     Dashboard
                 </span>
             )}
             {pathname === "/orders" && (
-                <span className="text-[24px] font-[600] leading-none">
+                <span className="text-[36px] font-[600] leading-none">
                     Orders
                 </span>
             )}
             {pathname === "/customers" && (
-                <span className="text-[24px] font-[600] leading-none">
+                <span className="text-[36px] font-[600] leading-none">
                     Customers
                 </span>
             )}
             {pathname === "/products" && (
-                <span className="text-[24px] font-[600] leading-none">
+                <span className="text-[36px] font-[600] leading-none">
                     Products
                 </span>
             )}
             {pathname === "/stocks" && (
-                <span className="text-[24px] font-[600] leading-none">
+                <span className="text-[36px] font-[600] leading-none">
                     Stocks
                 </span>
             )}
