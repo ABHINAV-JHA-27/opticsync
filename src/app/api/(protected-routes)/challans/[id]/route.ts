3e453fb6a8d75adaa78f0f39cd10e9b4aa458802
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-03-08 03:14:56 +0530

    Added challan by id route

--- src/app/api/(protected-routes)/challans/[id]/route.ts
+++ src/app/api/(protected-routes)/challans/[id]/route.ts
@@ -1,12 +1,36 @@
 import dbConnection from "@/lib/dbConnect";
 import Challan from "@/models/challan";
 import { NextRequest, NextResponse } from "next/server";
+import ejs from "ejs";
+import User from "@/models/user";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function GET(
     req: NextRequest,
     { params }: { params: { id: string } }
 ) {
+    const { isAuthenticated, getUser } = getKindeServerSession();
+
+    if (!(await isAuthenticated())) {
+        return NextResponse.json({
+            status: 401,
+            message: "Unauthorized",
+        });
+    }
+
     await dbConnection();
+
+    const kindeUser = await getUser();
+
+    const user = await User.findOne({ kindeUserId: kindeUser?.id });
+
+    if (!user) {
+        return NextResponse.json({
+            status: 404,
+            message: "User not found",
+        });
+    }
+
     const challans = await Challan.findById(params.id)
         .populate("orders")
         .populate("products")
@@ -19,8 +43,17 @@ export async function GET(
         });
     }
 
+    const challanHtml = await ejs.renderFile("src/views/challan.ejs", {
+        challanNumber: challans.challanNumber,
+        date: challans.date,
+        customer: challans.customer,
+        orders: challans.orders,
+        user: user,
+        total: challans.total,
+    });
+
     return NextResponse.json({
-        data: challans,
+        data: challanHtml,
         status: 200,
     });
 }
