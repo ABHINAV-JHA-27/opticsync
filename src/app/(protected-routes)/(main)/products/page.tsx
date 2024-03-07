7c5db9afb6a228b994ccfcc3fff659910f839746
ABHINAV-JHA-27 <abhinavjha@Abhinavs-MacBook-Air.local>
2024-03-08 02:27:42 +0530

    removed chart component

--- src/app/(protected-routes)/(main)/products/page.tsx
+++ src/app/(protected-routes)/(main)/products/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
     return (
         <div className="h-[100%] p-4">
             <div className="flex items-center justify-between mt-4 w-full">
-                <TopProducts />
-                <TopCompanyByProducts />
+                {/* <TopProducts />
+                <TopCompanyByProducts /> */}
             </div>
             <ProductTable />
         </div>
