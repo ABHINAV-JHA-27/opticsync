384eb6543a92794c00c2effbdf70bf03f5304b5c
Abhinav Jha <jha.abhinav2710@gmail.com>
2024-03-10 16:55:41 +0530

    removed auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 import { NextRequest } from "next/server";
 
 export default function middleware(req: NextRequest) {
-    return withAuth(req);
+    return;
+    // return withAuth(req);
 }
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
